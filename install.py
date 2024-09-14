cd383fc8cbc5f21ff5d872b3d80b6b277ccce725
Vince <phucvn166@gmail.com>
2024-09-14 12:38:15 -0400

    Install cohere

--- install.py
+++ install.py
@@ -1,11 +1,12 @@
 import cohere
 
-co = cohere.Client(api_key="<YOUR API KEY>")
+co = cohere.Client('COHERE_API_KEY')
 
 response = co.chat(
     model="command-r-plus",
 	message="Write me a short summary for this person with several bullet points for interesting points about them.",
     conversation_id='user_id',
+    document = """"""
 )
 
 print(response.text)
