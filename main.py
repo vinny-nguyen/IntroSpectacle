a377a52d5510535c8233b2f6ef9b207bc9856155
Erkam Boyacioglu <boyaciogluerkam@gmail.com>
2024-09-14 03:18:45 -0400

    Takes Picture when pressed q

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
-import numpy  as np
+import numpy as np
 import cv2 as cv
+import time
 
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+def main():
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
- 
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Display the resulting frame
-    cv.imshow('frame',frame)
-    if cv.waitKey(1) == ord('q'):
-        break
- 
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+    tap = 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        cv.imshow('Live Feed', frame)
+
+        key = cv.waitKey(1) & 0xFF
+        if key == ord('q'):
+            tap = 1
+        elif key == 27:  # 'Esc' key to exit
+            break
+
+        if tap == 1:
+            # Capture and save a picture
+            image_filename = f'person.png'
+            cv.imwrite(image_filename, frame)
+            print(f"Image saved as {image_filename}")
+            tap = 0
+            continue
+
+    cap.release()
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
